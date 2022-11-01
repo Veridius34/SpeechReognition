6419c6a7f4a1ad3b80b1c77f223d5923f5413f84
Veridius34 <88434057+Veridius34@users.noreply.github.com>
2022-11-01 16:07:43 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+using System.Globalization;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+       // private static ;
+        static void Main(string[] args)
+        {
+            
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+            sre.SetInputToDefaultAudioDevice();
+            sre.LoadGrammar(new DictationGrammar());
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+            sre.SpeechRecognized += rec;
+            
+            /*
+            foreach (RecognizerInfo ri in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                System.Diagnostics.Debug.WriteLine(ri.Culture.Name);
+            }*/
+            Console.ReadKey();
+        }
+        private static void rec(object sender,SpeechRecognizedEventArgs result) {
+            Console.WriteLine(result.Result.Text);
+
+        }
+    }
+}
